0b9b1b23987ebbb7dbdb81dc6fd4948c11706fa0
xiaojiaoya <937741304@qq.com>
2019-01-14 13:20:43 +0800

    加入morgan中间件

--- app.js
+++ app.js
@@ -2,16 +2,18 @@ const express = require('express'),
     app = express(),
     bodyParser = require('body-parser'),
     {join} = require('path'),
+    morgan = require('morgan'),
     {port} = require('./config');
 
 
+app.use(morgan('dev'));
 
 app.all('*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
   res.header("X-Powered-By",' 3.2.1');
-  res.header("Content-Type", "application/json;charset=utf-8");
+  // res.header("Content-Type", "application/json;charset=utf-8");
   next();
 });
 
