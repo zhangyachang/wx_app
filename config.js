75b89a9471a5b91b1d940d097cff1248db41d7ed
xiaojiaoya <937741304@qq.com>
2019-01-14 13:49:11 +0800

    缓存了access_token

--- config.js
+++ config.js
@@ -4,7 +4,9 @@ module.exports = {
     appId: "wx5a6f4750ba3ee7e6",
     secret: "096707e8c3caefa57b83186cf9875d7c",
     templateId: "INY2z45CUuptLDZ_lF8AcSmIve8thiTUtinLxJ"
-  }
+  },
+  access_token: '',
+  
 };
 
 
@@ -14,5 +16,4 @@ module.exports = {
   secret : 096707e8c3caefa57b83186cf9875d7c
   模板id : dj-INY2z45CUuptLDZ_lF8AcSmIve8thiTUtinLxJ-E
   
-
  */
--- router/index.js
+++ router/index.js
@@ -1,9 +1,10 @@
 const express = require('express'),
+    axios = require('axios'),
     router = express.Router();
 
 
-const {appInfo} = require('../config');
-
+const {appInfo, access_token} = require('../config');
+const config = require('../config');
 
 
 router.get('/', (req, res) => {
@@ -12,14 +13,23 @@ router.get('/', (req, res) => {
 
 // 获取token
 router.get('/token', (req, res) => {
-  axios.get(`https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${appInfo.appId}&secret=${appInfo.secret}`)
-      .then(res => {
-        console.log(res);
-        res.send(res);
-      })
-      .catch(err => {
-        console.log(err);
-      });
+  if(access_token){
+    res.send(access_token);
+  }else{
+    axios.get(`https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${appInfo.appId}&secret=${appInfo.secret}`)
+        .then(msg => {
+          config.access_token = msg.data.access_token;
+          res.send(config.access_token);
+        })
+        .catch(err => {
+          console.log(err);
+          res.send({
+            status: 400,
+            msg: '请求错误'
+          })
+        });
+  }
+  
 });
 
 
