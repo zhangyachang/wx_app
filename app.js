4c56d8d947bc792e6f91a82f3764990220f08d79
centos <937741304@qq,com>
2019-01-21 15:55:40 +0800

    设置图片服务

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.all('*', function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
   res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
   res.header("X-Powered-By",' 3.2.1');
-  res.header("Content-Type", "application/json;charset=utf-8")
+  // res.header("Content-Type", "application/json;charset=utf-8")
   if(req.method=="OPTIONS") res.send(200);
   else next()
 });
--- config/wx_config.js
+++ config/wx_config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  port: 3000,
+  port: 8888,
   appInfo: {
     appId: "wx5a6f4750ba3ee7e6",
     secret: "096707e8c3caefa57b83186cf9875d7c",
