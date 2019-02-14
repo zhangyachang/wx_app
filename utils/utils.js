67a401bcedc08774629abd0808590da3d1b91a60
xiaojiaoya <937741304@qq.com>
2019-02-14 09:16:44 +0800

    完善解密工具函数

--- utils/utils.js
+++ utils/utils.js
@@ -19,9 +19,10 @@ exports.sha1 = function (...arr) {
 
 /*
     @explain: 微信的消息密文解密方法
-    @version 1.0.0
+    @version 1.0.1
+        修复部分消息解析失败的情况
     @author: Z
-    @data :2019-02-13
+    @data :2019-02-14
     @params:
         obj.AESKey:解密的aesKey值
         obj.text: 需要解密的密文
@@ -31,41 +32,20 @@ exports.sha1 = function (...arr) {
         obj.noncestr  随机数
         obj.msg_len   微信密文的len
         obj.msg       解密后的明文
-        obj.corpid    企业id
 */
 
 exports.decrypt = function (obj, type) {
-    try{
-        let aesKey = Buffer.from(obj.AESKey + '=', 'base64');
-        const cipherEncoding = 'base64';
-        const clearEncoding = 'utf8';
-        console.log('第一步');
-        console.log('aesKey===》',aesKey);
-        const cipher = crypto.createDecipheriv('aes-256-cbc',aesKey,aesKey.slice(0, 16));
-        
-        
-        // if(type == 'msg'){
-            cipher.setAutoPadding(false);
-        // }
-        console.log('第二步');
-        let this_text = cipher.update(obj.text, cipherEncoding, clearEncoding) + cipher.final(clearEncoding);
-        console.log('第三部');
-        console.log(this_text);
-        
-        console.log('第四部');
-        var rg=/$"}"/g;
-        console.log("fsdf}sf")
-        return {
-            noncestr:this_text.substring(0,16),
-            msg_len:this_text.substring(16,20),
-            msg:this_text.substring(20,this_text.lastIndexOf("}")+1),
-            corpid:this_text.substring(this_text.length-obj.corpid.length,this_text.length)
-        }
-    }catch (e) {
-        console.log('捕获到的错误信息');
-        console.log(e);
+    let aesKey = Buffer.from(obj.AESKey + '=', 'base64');
+    const cipherEncoding = 'base64';
+    const clearEncoding = 'utf8';
+    const cipher = crypto.createDecipheriv('aes-256-cbc',aesKey,aesKey.slice(0, 16));
+    cipher.setAutoPadding(false); // 是否取消自动填充 不取消
+    let this_text = cipher.update(obj.text, cipherEncoding, clearEncoding) + cipher.final(clearEncoding);
+    return {
+        noncestr:this_text.substring(0,16),
+        msg_len:this_text.substring(16,20),
+        msg:this_text.substring(20,this_text.lastIndexOf("}")+1)
     }
-    
 };
 
 
